61c08e299e396a955ba24f2cd9e67c57b55614c3
Clapa Lucian <lucian@dealdash.com>
2017-02-13 00:03:25 +0200

    remove products

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,15 +11,6 @@ var user = require('./users.js');
 router.post('/api/v1/login', auth.login);
 
 /*
- * Routes that can be accessed only by autheticated users
- */
-router.get('/api/v1/products', products.getAll);
-router.get('/api/v1/product/:id', products.getOne);
-router.post('/api/v1/product/', products.create);
-router.put('/api/v1/product/:id', products.update);
-router.delete('/api/v1/product/', products.delete);
-
-/*
  * Routes that can be accessed only by authenticated & authorized users
  */
 router.get('/api/v1/users', user.getAll);

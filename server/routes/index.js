13f9c0507c621295d2301b8ac3fcba513e5d889f
Clapa Lucian <lucian@dealdash.com>
2017-02-13 00:12:16 +0200

    update package

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var auth = require('./auth.js');
-var products = require('./products.js');
 var user = require('./users.js');
 
 /*

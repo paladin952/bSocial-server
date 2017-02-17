393b05c163714d89c9f208ab9dbde7132270675f
Clapa Lucian <lucian@dealdash.com>
2017-02-17 23:44:36 +0200

    reuse x token if not expired

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -28,16 +28,41 @@ var auth = {
                     "message": "Invalid credentials"
                 });
             } else {
-                res.status(200);
-                var loginModel = genToken(dbUserObj);
-                userManager.updateToken(req.db, loginModel.user.username, loginModel.token, function () {
-                });
-                res.json(loginModel);
+                if (dbUserObj.token) {
+                    console.log("token exists");
+                    var loginModel = {};
+                    var decodedToken = decodeToken(dbUserObj.token);
+                    console.log(decodedToken);
+                    if (decodedToken && !isTokenExpired(decodedToken)) {
+                        console.log("token exists and not expired");
+                        loginModel.token = dbUserObj.token;
+                        loginModel.expires = decodedToken.expires;
+                        loginModel.user = dbUserObj;
+
+                        res.status(200);
+                        res.json(loginModel);
+                    } else {
+                        console.log("token expired or coudn not decode");
+                        generateTokenAndRespond(req, res, dbUserObj);
+                    }
+
+                } else {
+                        console.log("token expired sau not exists 222");
+                    generateTokenAndRespond(req, res, dbUserObj);
+                }
             }
 
         });
     }
 };
+
+function generateTokenAndRespond(req, res, dbUserObj) {
+    var loginResponseModel = genToken(dbUserObj);
+    userManager.updateToken(req.db, loginResponseModel.user.username, loginResponseModel.token, function () {
+    });
+    res.status(200);
+    res.json(loginResponseModel);
+}
 
 function genToken(user) {
     var expires = expiresIn(365); // 7 days
@@ -55,6 +80,19 @@ function genToken(user) {
 function expiresIn(numDays) {
     var dateObj = new Date();
     return dateObj.setDate(dateObj.getDate() + numDays);
+}
+
+function decodeToken(token) {
+    try {
+        return jwt.decode(token, require('../config/secret.js')());
+    } catch (err) {
+        return null;
+    }
+
+}
+
+function isTokenExpired(token) {
+    return token.exp <= Date.now();
 }
 
 module.exports = auth;
